58a2d4e1357fb506c93062577cf85a084c09dc5d
night <110598750+nighteraser@users.noreply.github.com>
2023-02-21 10:52:21 +0800

    Update main.c

--- main.c
+++ main.c
@@ -13,17 +13,35 @@ char board[8][8] = {
 char blackchess[6] = { 'r', 'n', 'b', 'q', 'k', 'p' };
 char whitechess[6] = { 'R', 'N', 'B', 'Q', 'K', 'P' };
 
-void draw_board(){
+
+void draw_board() {
     int i, j;
-    for (i = 0; i < 8; i++){
-        for (j = 0; j < 8; j++){
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
             printf("%c", board[i][j]);
         }
         printf("\n");
     }
 }
 
-int main(){
-    draw_board();
+int main() {
+    int a, b, c, d;
+
+    printf("Please choose the chess position: \n");
+    while (1) {
+        scanf_s("%d %d", &a, &b, 1);
+        if (a >= 0 && a < 8 && b >= 0 && b < 8) break;
+        printf("Please enter the correct chess position\n");
+    }
+    printf("Please choose the end position: \n");
+    while (1) {
+        scanf_s("%d %d", &c, &d, 1);
+        if (c >= 0 && c < 8 && d >= 0 && d < 8) break;
+        printf("Please enter the correct end position\n");
+    }
+    
+
+
+
     return 0;
 }
