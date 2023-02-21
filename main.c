2ec7178ca5bd0e41ef3a7709c042556f1880f2af
night <110598750+nighteraser@users.noreply.github.com>
2023-02-21 10:17:46 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+char board[8][8] = {
+    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+    {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+};
+char blackchess[6] = { 'r', 'n', 'b', 'q', 'k', 'p' };
+char whitechess[6] = { 'R', 'N', 'B', 'Q', 'K', 'P' };
+
+void draw_board(){
+    int i, j;
+    for (i = 0; i < 8; i++){
+        for (j = 0; j < 8; j++){
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main(){
-    
+    draw_board();
     return 0;
 }
